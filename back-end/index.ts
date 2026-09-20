3a981ac3f28a822135c8902285e8f56cce10f300
Jorge Risso <jorge.risso00@gmail.com>
2026-09-20 09:13:24 -0300

    finalmente

--- back-end/index.ts
+++ back-end/index.ts
@@ -6,7 +6,10 @@ import { router } from "./src/routes.js";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cada-do-hamburguer.vercel.app",
+];
 
 app.use(express.json({ limit: "10mb" }));
 
@@ -22,18 +25,14 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: (origin, callback) => {
-      // Permite requisições sem Origin
-      // (ex: Postman ou chamadas internas)
       if (!origin) {
         return callback(null, true);
       }
 
-      // Permite localhost
-      if (origin === "http://localhost:5173") {
+      if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
 
-      // Permite qualquer URL do projeto Vercel
       if (
         origin.startsWith("https://cada-do-hamburguer-") &&
         origin.endsWith(".vercel.app")
