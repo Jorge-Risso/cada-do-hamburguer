fbf90f4fa65982d6d43a47e854a7a0de0ef744cd
Jorge Risso <jorge.risso00@gmail.com>
2026-09-20 08:55:43 -0300

    agora vai final

--- back-end/index.ts
+++ back-end/index.ts
@@ -7,12 +7,20 @@ import { router } from "./src/routes.js";
 const app = express();
 
 const allowedOrigins = [
-  "http://fetch(`${import.meta.env.VITE_API_URL}:5173",
+  "http://localhost:5173",
   "https://cada-do-hamburguer-git-main-project00s-projects.vercel.app",
+  "https://cada-do-hamburguer-6u2tz98q3-project00s-projects.vercel.app",
 ];
 
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10mb",
+  }),
+);
+
 app.use(cookieParser());
 
 app.use(
